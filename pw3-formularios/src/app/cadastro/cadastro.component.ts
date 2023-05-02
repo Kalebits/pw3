9abc4af6a8e954a0e863960f424c08ade2b78faa
Ana-Kaleb <Ana-Kaleb@example.com>
2023-05-02 11:01:15 -0300

    formularios

---
+++ pw3-formularios/src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent {
+
+  cadastroForm: FormGroup;
+
+  constructor(private builder:FormBuilder){
+      this.cadastroForm = builder.group({nome:[''],
+    usuario:[''],
+    email:[''],
+    senha:[''],
+    confirmarsenha:[''],
+    aceite:[false]
+  })
+  }
+
+  cadastrar(){
+    console.log(this.cadastroForm.value);
+  }
+
+}
