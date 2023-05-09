ec4ed96c0cb89d31769789ebcd942be24d4125f4
Ana-Kaleb <Ana-Kaleb@example.com>
2023-05-09 11:02:49 -0300

    Validações

--- pw3-formularios/src/app/cadastro/cadastro.component.ts
+++ pw3-formularios/src/app/cadastro/cadastro.component.ts
@@ -1,27 +1,26 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  styleUrls: ['./cadastro.component.css'],
 })
 export class CadastroComponent {
-
   cadastroForm: FormGroup;
 
-  constructor(private builder:FormBuilder){
-      this.cadastroForm = builder.group({nome:[''],
-    usuario:[''],
-    email:[''],
-    senha:[''],
-    confirmarsenha:[''],
-    aceite:[false]
-  })
+  constructor(private builder: FormBuilder) {
+    this.cadastroForm = builder.group({
+      nome: [''],
+      usuario: ['', Validators.required],
+      email: ['', Validators.required],
+      senha: ['', Validators.required],
+      confirmarsenha: [''],
+      aceite: [false, [Validators.requiredTrue]],
+    });
   }
 
-  cadastrar(){
+  cadastrar() {
     console.log(this.cadastroForm.value);
   }
-
 }
